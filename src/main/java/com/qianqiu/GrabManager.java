ab35d28af26505dee400a1a03102680b198424c9
QianQ77 <qianqiucs@gmail.com>
2018-08-07 11:40:40 -0400

    Add GrabManager and its test class; Modify GrabPage to maintain a level field

---
+++ src/main/java/com/qianqiu/GrabManager.java
@@ -0,0 +1,7 @@
+package com.qianqiu;
+
+/**
+ * Created by qiuqian on 8/6/18.
+ */
+public class GrabManager {
+}
---
+++ src/test/java/com/qianqiu/TestGrabManager.java
@@ -0,0 +1,7 @@
+package com.qianqiu;
+
+/**
+ * Created by qiuqian on 8/7/18.
+ */
+public class TestGrabManager {
+}
